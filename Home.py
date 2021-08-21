1b11e29427dfd181f815862e540bd0e2da154689
LeonardoMateini <69974624+LeonardoMateini@users.noreply.github.com>
2021-08-20 22:22:25 -0300

    Add files via upload

--- Home.py
+++ Home.py
@@ -7,13 +7,22 @@ banco = mysql.connector.connect(
     passwd='',
     database="DB_KBT_SYSTEM"
 )
+# cursor = banco.cursor()
+# comando_SQL = """create table SERVICO (
+# 	ID INT NOT NULL AUTO_INCREMENT,
+# 	DESCRICAO varchar (50),
+# 	PRECO DOUBLE,
+# 	PRIMARY KEY (id)
+# );"""
+# cursor.execute(comando_SQL)
+# banco.commit()
 
-cursor = banco.cursor()
 
 def chamar_cadastro():
     ambiente_cadastro.show()
     ambiente_cadastro.pushButton_3.clicked.connect(chamar_novo_cliente)
     ambiente_cadastro.pushButton_5.clicked.connect(chamar_novo_produto_servico)
+
 
 def chamar_novo_cliente():
     novo_cliente.show()
@@ -23,19 +32,46 @@ def chamar_agenda():
     ambiente_agenda.show()
     ambiente_agenda.pushButton_2.clicked.connect(chamar_novo_agendamento)
 
+
 def chamar_novo_agendamento():
     novo_agendamento.show()
 
+
 def chamar_novo_produto_servico():
     novo_produto_serviço.show()
+    novo_produto_serviço.pushButton_2.clicked.connect(adicionar_produto)
+    novo_produto_serviço.pushButton_5.clicked.connect(adicionar_servico)
+
+def adicionar_produto():
+    descricao = novo_produto_serviço.lineEdit_2.text()
+    codigo_barras = novo_produto_serviço.lineEdit_3.text()
+    preco = novo_produto_serviço.lineEdit_4.text()
+    fornecedor = novo_produto_serviço.lineEdit_5.text()
+    marca = novo_produto_serviço.lineEdit_6.text()
+
+    cursor = banco.cursor()
+    # verificacao_id_ja_preenchido = "SELECT TOP 1 * FROM "
+    comando_SQL = "INSERT INTO TB_PRODUTO (DESCRICAO, CODIGO_BARRAS, PRECO, FORNECEDOR, MARCA) VALUES (%s,%s,%s,%s,%s)"
+    dados = (str(descricao), str(codigo_barras), preco, str(fornecedor), str(marca))
+    cursor.execute(comando_SQL, dados)
+    banco.commit()
+
+def adicionar_servico():
+    descricao = novo_produto_serviço.lineEdit_10.text()
+    preco = novo_produto_serviço.lineEdit_11.text()
+
+    cursor = banco.cursor()
+    # verificacao_id_ja_preenchido = "SELECT TOP 1 * FROM "
+    comando_SQL = "INSERT INTO TB_SERVICO (DESCRICAO, PRECO) VALUES (%s,%s)"
+    dados = (str(descricao), str(preco))
+    cursor.execute(comando_SQL, dados)
+    banco.commit()
 
 app = QtWidgets.QApplication([])
 home = uic.loadUi('Home.ui')
 home.pushButton.clicked.connect(chamar_agenda)
 home.pushButton_3.clicked.connect(chamar_cadastro)
 home.show()
-
-
 
 ambiente_agenda = uic.loadUi('Agenda.ui')
 novo_agendamento = uic.loadUi('Novo Agendamento.ui')
