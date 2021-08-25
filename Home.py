5766cec209ec62e99e050b03bd92200c69653c57
Daniel Amorim <amoorimdaniel@gmail.com>
2021-08-25 17:09:00 -0300

    editar, excluir, salvar serviço

--- Home.py
+++ Home.py
@@ -4,7 +4,8 @@ import mysql.connector
 from mysql.connector import connect, cursor
 from reportlab.pdfgen import canvas
 
-valor_id= 0
+valor_id_produto = 0
+valor_id_servico = 0
 
 banco = mysql.connector.connect(
     host="localhost",
@@ -13,8 +14,8 @@ banco = mysql.connector.connect(
     database="kbt_system"
 )
 
-def editar_dados():
-    global valor_id
+def editar_dados_produtos():
+    global valor_id_produto
     linha = listar_dados_produtos.tableWidget.currentRow()
 
     cursor = banco.cursor()
@@ -25,7 +26,7 @@ def editar_dados():
     produto = cursor.fetchall()
     editar_produto.show()
 
-    valor_id = id_valor
+    valor_id_produto = id_valor
 
     editar_produto.lineEdit.setText(str(produto[0][0]))
     editar_produto.lineEdit_2.setText(str(produto[0][1]))
@@ -34,9 +35,42 @@ def editar_dados():
     editar_produto.lineEdit_5.setText(str(produto[0][4]))
     editar_produto.lineEdit_6.setText(str(produto[0][5]))
 
+def editar_dados_servico():
+    global valor_id_servico
+    linha = listar_dados_servico.tableWidget.currentRow()
+
+    cursor = banco.cursor()
+    cursor.execute("SELECT id FROM servico")
+    id_lidos = cursor.fetchall()
+    id_valor = id_lidos[linha][0]
+    cursor.execute("SELECT * FROM servico WHERE id="+str(id_valor))
+    servico = cursor.fetchall()
+    editar_servico.show()
+
+    valor_id_servico = id_valor
+
+    editar_servico.lineEdit_3.setText(str(servico[0][0]))
+    editar_servico.lineEdit_4.setText(str(servico[0][1]))
+    editar_servico.lineEdit_5.setText(str(servico[0][2]))
+
+def salvar_servico_editado():
+    # pegar o id
+    global valor_id_servico
+    # pegar o que foi digitado na linha
+    descricao = editar_servico.lineEdit_4.text()
+    preco = editar_servico.lineEdit_5.text()
+    # atualizar os dados no banco
+    salvar = banco.cursor()
+    salvar.execute("UPDATE servico SET descricao='{}', preco='{}' WHERE id={}".format(descricao,preco,valor_id_servico))
+    #fechar e atualizar dados
+    editar_servico.close()
+    listar_dados_servico.close()
+    listar_servico()
+
+
 def salvar_produto_editado():
     # pegar o id
-    global valor_id
+    global valor_id_produto
     # pegar o que foi digitado na linha
     descricao = editar_produto.lineEdit_2.text()
     codigo_de_barras = editar_produto.lineEdit_3.text()
@@ -45,13 +79,13 @@ def salvar_produto_editado():
     marca = editar_produto.lineEdit_6.text()
     # atualizar os dados no banco
     salvar = banco.cursor()
-    salvar.execute("UPDATE produto SET descricao='{}', codigo_de_barras='{}', preco='{}', fornecedor='{}', marca='{}' WHERE id={}".format(descricao,codigo_de_barras,preco,fornecedor,marca,valor_id))
+    salvar.execute("UPDATE produto SET descricao='{}', codigo_de_barras='{}', preco='{}', fornecedor='{}', marca='{}' WHERE id={}".format(descricao,codigo_de_barras,preco,fornecedor,marca,valor_id_produto))
     #fechar e atualizar dados
     editar_produto.close()
     listar_dados_produtos.close()
     listar_produtos()
 
-def excluir_dados():
+def excluir_dados_produtos():
     linha = listar_dados_produtos.tableWidget.currentRow()
     listar_dados_produtos.tableWidget.removeRow(linha)
 
@@ -61,7 +95,17 @@ def excluir_dados():
     id_valor = id_lidos[linha][0]
     excluir.execute("DELETE FROM produto WHERE id=" + str(id_valor))
 
-def gerar_pdf():
+def excluir_dados_servico():
+    linha = listar_dados_servico.tableWidget.currentRow()
+    listar_dados_servico.tableWidget.removeRow(linha)
+
+    excluir = banco.cursor()
+    excluir.execute("SELECT id FROM servico")
+    id_lidos = excluir.fetchall()
+    id_valor = id_lidos[linha][0]
+    excluir.execute("DELETE FROM servico WHERE id=" + str(id_valor))
+
+def gerar_pdf_produto():
     tb_produtos = banco.cursor()
     comando_SQL = "SELECT * FROM produto"
     tb_produtos.execute(comando_SQL)
@@ -89,7 +133,30 @@ def gerar_pdf():
         pdf.drawString(530,750 - y, str(dados_lidos[i][5]))
 
     pdf.save()
-    print("PDF FOI GERADO COM SUCESSO!")
+
+def gerar_pdf_servico():
+
+    tb_servicos = banco.cursor()
+    comando_SQL = "SELECT * FROM servico"
+    tb_servicos.execute(comando_SQL)
+    dados_lidos = tb_servicos.fetchall() 
+    y = 0
+    pdf = canvas.Canvas("Serviços_cadastrados.pdf")
+    pdf.setFont("Times-Bold", 12)
+    pdf.drawString(200,800, "Serviços cadastrados:")
+    pdf.setFont("Times-Bold", 12)
+
+    pdf.drawString(5,750, "ID")
+    pdf.drawString(40,750, "DESCRIÇÃO")
+    pdf.drawString(160,750, "PREÇO")
+
+    for i in range (0, len(dados_lidos)):
+        y = y + 50
+        pdf.drawString(5,750 - y, str(dados_lidos[i][0]))
+        pdf.drawString(60,750 - y, str(dados_lidos[i][1]))
+        pdf.drawString(180,750 - y, str(dados_lidos[i][2]))
+
+    pdf.save()    
 
 def add_produto():
 
@@ -140,6 +207,22 @@ def listar_produtos():
         for j in range(0, 6):
             listar_dados_produtos.tableWidget.setItem(i,j,QtWidgets.QTableWidgetItem(str(dados_lidos[i][j])))
 
+def listar_servico():
+    listar_dados_servico.show()
+
+    tb_servico = banco.cursor()
+    comando_SQL = "SELECT * FROM servico"
+    tb_servico.execute(comando_SQL)
+    dados_lidos = tb_servico.fetchall()  
+
+    listar_dados_servico.tableWidget.setRowCount(len(dados_lidos))
+    listar_dados_servico.tableWidget.setColumnCount(3) 
+
+    for i in range (0,len(dados_lidos)):
+        for j in range(0, 3):
+            listar_dados_servico.tableWidget.setItem(i,j,QtWidgets.QTableWidgetItem(str(dados_lidos[i][j])))
+
+
 def chamar_cadastro():
     ambiente_cadastro.show()
     ambiente_cadastro.pushButton_3.clicked.connect(chamar_novo_cliente)
@@ -181,18 +264,29 @@ novo_cliente = uic.loadUi('Novo Cliente.ui')
 
 listar_dados_produtos = uic.loadUi('Listar dados produtos.ui')
 
+listar_dados_servico = uic.loadUi('Lista dados serviço.ui')
+
 editar_produto = uic.loadUi('Editar produto.ui')
+
+editar_servico =uic.loadUi('Editar serviço.ui')
 
 novo_produto_serviço = uic.loadUi('Novo Produto e Serviço.ui')
 novo_produto_serviço.pushButton_2.clicked.connect(add_produto)
-novo_produto_serviço.pushButton_5.clicked.connect(add_servico)
+novo_produto_serviço.pushButton_3.clicked.connect(add_servico)
 novo_produto_serviço.pushButton.clicked.connect(listar_produtos)
+novo_produto_serviço.pushButton_4.clicked.connect(listar_servico)
 
-listar_dados_produtos.pushButton.clicked.connect(gerar_pdf)
-listar_dados_produtos.pushButton_2.clicked.connect(excluir_dados)
-listar_dados_produtos.pushButton_3.clicked.connect(editar_dados)
+listar_dados_produtos.pushButton.clicked.connect(gerar_pdf_produto)
+listar_dados_produtos.pushButton_2.clicked.connect(excluir_dados_produtos)
+listar_dados_produtos.pushButton_3.clicked.connect(editar_dados_produtos)
+
+listar_dados_servico.pushButton.clicked.connect(gerar_pdf_servico)
+listar_dados_servico.pushButton_2.clicked.connect(excluir_dados_servico)
+listar_dados_servico.pushButton_3.clicked.connect(editar_dados_servico)
 
 editar_produto.pushButton.clicked.connect(salvar_produto_editado)
+
+editar_servico.pushButton.clicked.connect(salvar_servico_editado)
 
 home.show()
 app.exec()
